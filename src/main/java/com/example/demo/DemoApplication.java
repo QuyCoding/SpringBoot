3f4278218b4537f9f4825619e9b09b40a3234113
quyhoangs <quyromutc@gmail.com>
2024-07-18 21:40:44 +0700

    Hello Word

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,12 +2,28 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
 
+@RestController
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+	
+	//Annotation này ánh xạ các request HTTP GET đến method hello. 
+	// Khi một request GET được gửi đến root URL ("/"), method này sẽ được gọi.
+	// @GetMapping("/")
+	// public String hello() {
+	// 	return "Hello World!";
+	// }
+	
+	//trả về 1 api json
+	@GetMapping("/api")
+	public String api() {
+		return "{ \"name\": \"Thanh\", \"age\": 20 }";
 	}
 
 }
