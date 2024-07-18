3abfb9b538847e03a9018f9f860a5f7ed5d42d3e
quyhoangs <quyromutc@gmail.com>
2024-07-18 22:54:20 +0700

    Tạo kiến trúc Service

--- src/main/java/com/example/demo/Student/StudentController.java
+++ src/main/java/com/example/demo/Student/StudentController.java
@@ -1,7 +1,5 @@
 package com.example.demo.Student;
 
-import java.time.LocalDate;
-import java.time.Month;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,21 +10,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(path = "api/v1/student")
 public class StudentController {
     
+    private final StudentService studentService;
+    
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
+    }
+    
     // @GetMapping("/"): Ánh xạ các request GET đến URL gốc ("/").
 	// Phương thức này trả về danh sách các đối tượng Student dưới dạng JSON. 
 	// Trong ví dụ này, danh sách chỉ có một sinh viên với thông tin được cung cấp.
 	//trả về 1 api json
-	@GetMapping()
+	@GetMapping
 	public List<Student> getStudents() {
-		return List.of(
-			new Student(
-				1L,
-				"Maria",
-				"Maria@gmail.com",
-				LocalDate.of(2000, Month.JANUARY, 5),
-				21
-			)
-		);
+        return studentService.getStudents();
 	}
 
 }
