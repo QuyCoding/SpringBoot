38da15b8774ed28b35509d656bda46fb4fe03656
quyhoangs <quyromutc@gmail.com>
2024-07-21 23:25:10 +0700

    Tạo dữ liệu mẫu bằng Repository, Khi chạy mvn spring-boot:run  thì tự động create

---
+++ src/main/java/com/example/demo/Student/StudentConfig.java
@@ -0,0 +1,39 @@
+package com.example.demo.Student;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Bean;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@Configuration
+public class StudentConfig {
+    
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository studentRepository) {
+        return args -> {
+            Student alex = new Student(
+                "Alex",
+                "Alex@gmail.com",
+                LocalDate.of(2000, Month.JANUARY, 5),
+                21
+            );
+            
+            Student maria = new Student(
+                "maria",
+                "maria@gmail.com",
+                LocalDate.of(2000, Month.JANUARY, 5),
+                22
+            );
+            
+            studentRepository.saveAll(
+                List.of(alex, maria)
+            );
+            
+            
+        };
+    }            
+
+}
--- src/main/java/com/example/demo/Student/StudentService.java
+++ src/main/java/com/example/demo/Student/StudentService.java
@@ -16,16 +16,6 @@ public class StudentService {
 	}
 	
     public List<Student> getStudents() {
-		// return List.of(
-		// 	new Student(
-		// 		1L,
-		// 		"Maria",
-		// 		"Maria@gmail.com",
-		// 		LocalDate.of(2000, Month.JANUARY, 5),
-		// 		21
-		// 	)
-		// );
-		
 		return studentRepository.findAll();
 	}
 	
