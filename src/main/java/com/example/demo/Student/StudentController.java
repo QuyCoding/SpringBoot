afd33a8fd2d0084dc8243c38e9c85c455baabc39
quyhoangs <quyromutc@gmail.com>
2024-07-25 22:34:44 +0700

    PostMapping

--- src/main/java/com/example/demo/Student/StudentController.java
+++ src/main/java/com/example/demo/Student/StudentController.java
@@ -6,6 +6,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 @RestController
 @RequestMapping(path = "api/v1/student")
@@ -30,5 +33,11 @@ public class StudentController {
 	public List<Student> getStudents() {
         return studentService.getStudents();
 	}
+    
+
+    @PostMapping
+    public void addNewStudent(@RequestBody Student student) {
+        studentService.addNewStudent(student);
+    }
 
 }
